295bfe160a0df243798434d7f25a47edd297c6e1
GAVINTENNESEN <gtennesen@gmail.com>
2022-02-19 21:21:37 -0600

    Initial commit #1

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ if playing != "yes":
     quit()
 if playing == "yes":
     print("Alright")
-#random_word = list["help", "hello", "how", "hit", "howdy"]
-random_word = "help"
+random_word = list["help", "hello", "how", "hit", "howdy"]
+tries = 6
+#random_word = "help"
 answer = None
 letters_in_word = list(random_word)
 letters_guessed = list()
@@ -22,9 +23,14 @@ while answer != random_word:
             print(letter)
         else:
             print('_')
-    if answer == random_word:
+    if letters_guessed == letters_in_word:
         print("Correct!! You got the whole word!!")
-    if answer != letters_in_word:
-        print("Try again")
-        if answer == letters_in_word:
-            print("You found a letter!!")
+        quit()
+    if answer not in letters_in_word:
+        tries -= 1
+        print("Try again, you have " + str(tries) + " tries.")
+    if answer in letters_in_word:
+        print("You found a letter!!")
+        if tries < 1:
+            print("Your out of tries!")
+            quit()
